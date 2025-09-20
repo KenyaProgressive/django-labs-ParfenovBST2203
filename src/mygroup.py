cdea018e808aaea230c6023829b6779e1742ce87
Nikita Parfenov (flatnosm) <n.a.parfenov@edu.mtuci.ru>
2025-09-20 12:03:03 +0300

    frist lab start

---
+++ src/mygroup.py
@@ -0,0 +1,14 @@
+def main():
+    
+    groupmates = [
+        {
+            "name": "Анастасия",
+            "surname": "Пахомова",
+            "exams": ["АиГ", "МИСПиСИТ", "АИС"],
+            "marks": [5, 5, 3]
+        }
+    ]
+
+
+if __name__ == "__main__":
+    main()
